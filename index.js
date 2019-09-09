dfeac599d22bc527cc49862313dc644ff5c56fe2
Clary Morla <michellemorla27@gmail.com>
2019-09-09 23:33:17 +0200

    Final commit

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ socket.emit('command-to-device',{msg: '*AA$PR1#'});
   
 console.log(socket);
   
-  socket.on('send-message', (message) => {  
-    io.emit('command-to-device', {msg: '*AA$PR1#', event: 'position-request'}); 
+  socket.on('command-to-device', (message) => {  
+    socket.emit('command-to-device',{msg: '*AA$PR1#'});
     console.log('------------');
     console.log(message);
     console.log('------------');
   });
 
   socket.on('command-from-device', (message) =>{
+    io.emit('confirmation', {msg: 'Mensaje Recibido en la Central', event: 'message_received'}); 
     console.log('------------');
     console.log(message);
     console.log('------------');
